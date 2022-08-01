c1a87f9402d7344c12e1018585e0abd577e66cba
DanK1368 <danielkoett@gmail.com>
2022-08-01 11:45:57 +0200

    About section implemented

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Header from "./components/Header";
 import Intro from "./components/Intro";
 import Work from "./components/Work";
+import About from "./components/About";
+import Portfolio from "./components/Portfolio";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
         <Header />
         <Intro />
         <Work />
+        <About />
+        <Portfolio />
       </div>
     </main>
   );
---
+++ src/components/About.js
@@ -0,0 +1,20 @@
+import { Button } from "./shared/Button";
+import profilePic from "../assets/image-amy.webp";
+
+const About = () => {
+  return (
+    <section className="flex flex-col justify-center items-center gap-6 text-center">
+      <img className=" w-[90%] " src={profilePic} alt="" />
+      <h3 className=" text-2xl font-semibold w-[90%] text-base-100 ">
+        I’m Amy, and I’d love to work on your next project
+      </h3>
+      <p className=" text-neutral-content leading-7 ">
+        I love working with others to create beautiful design solutions. I’ve
+        designed everything from brand illustrations to complete mobile apps.
+        I’m also handy with a camera!
+      </p>
+      <Button bgColor="bg-secondary" />
+    </section>
+  );
+};
+export default About;
